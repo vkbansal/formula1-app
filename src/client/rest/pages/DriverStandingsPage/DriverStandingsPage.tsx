36e4b80acc7cfd708e9fd6b519821480952857cf
Vivek Kumar Bansal <vkb0310@gmail.com>
2021-03-12 21:45:34 +0530

    add snippets

--- src/client/rest/pages/DriverStandingsPage/DriverStandingsPage.tsx
+++ src/client/rest/pages/DriverStandingsPage/DriverStandingsPage.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { Text } from '@chakra-ui/react';
+// import { DriverStandingsPage as View } from 'client/common/pages/DriverStandingsPage/DriverStandingsPage';
 
 export function DriverStandingsPage(): React.ReactElement {
+  const { driverRef, year } = useParams<{
+    driverRef: string;
+    year: string;
+  }>();
+
+  console.log({ driverRef, year });
+
   return (
     <React.Fragment>
       <Text fontSize="3xl">TODO: Implement this on React-Day</Text>
     </React.Fragment>
   );
 }
+
+/**
+
+const { data, loading } = useGet(
+  `/seasons/${year}/drivers/${driverRef}/standings`
+);
+
+const { data, loading } = useGetDriverStandings({ driverRef, year });
+
+  return data && !loading ? (
+    <View
+      module="rest"
+      name={`${data.forename} ${data.surname}`}
+      nationality={data.nationality}
+      year={year}
+      races={data.standings || []}
+    />
+  ) : (
+    <Spinner size="xl" />
+  );
+
+*/
