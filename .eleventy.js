f811129df66b50a109aebaefdaf7bf6e9231fb85
Vivek Kumar Bansal <vkb0310@gmail.com>
2022-06-05 23:07:39 +0530

    feat: add more details to season races

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 const dateFns = require('date-fns');
+const { snakeCase } = require('change-case');
 
 module.exports = (elevntyConfig) => {
   elevntyConfig.addNunjucksFilter('humanDate', function (value) {
     return dateFns.format(dateFns.parseISO(value), 'do LLL yyyy');
+  });
+
+  elevntyConfig.addNunjucksFilter('padLeft', function (value, ...args) {
+    return value.toString().padStart(...args);
+  });
+
+  elevntyConfig.addNunjucksFilter('snakeCase', function (value) {
+    return snakeCase(value || '');
   });
 
   return {
