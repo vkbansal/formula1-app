5a1ec1feb48d9c9e96a57716bc6db93d4f1f25b2
Vivek Kumar Bansal <vkb0310@gmail.com>
2022-07-07 10:28:18 +0530

    refactor: migrate to mariadb

--- src/common-client/sortable-table.js
+++ src/common-client/sortable-table.js
@@ -1,41 +1,41 @@
 /* global Alpine, tableData */
 document.addEventListener('alpine:init', () => {
-  function normalize(data) {
-    if (typeof data === 'string') {
-      return data.normalize('NFD');
-    }
+	function normalize(data) {
+		if (typeof data === 'string') {
+			return data.normalize('NFD');
+		}
 
-    return data;
-  }
+		return data;
+	}
 
-  Alpine.data('tableData', (initSortCol = null, initSortAsc = false) => ({
-    data: tableData,
-    sortCol: initSortCol,
-    sortAsc: initSortAsc,
-    init() {
-      if (initSortCol) {
-        this.sort(initSortCol, initSortAsc);
-      }
-    },
-    sort(col, sortAsc) {
-      if (typeof sortAsc === 'boolean') {
-        this.sortAsc = sortAsc;
-      } else {
-        this.sortAsc = this.sortCol === col ? !this.sortAsc : true;
-      }
+	Alpine.data('tableData', (initSortCol = null, initSortAsc = false) => ({
+		data: tableData,
+		sortCol: initSortCol,
+		sortAsc: initSortAsc,
+		init() {
+			if (initSortCol) {
+				this.sort(initSortCol, initSortAsc);
+			}
+		},
+		sort(col, sortAsc) {
+			if (typeof sortAsc === 'boolean') {
+				this.sortAsc = sortAsc;
+			} else {
+				this.sortAsc = this.sortCol === col ? !this.sortAsc : false;
+			}
 
-      this.sortCol = col;
-      this.data.sort((a, b) => {
-        if (normalize(a[col]) < normalize(b[col])) {
-          return this.sortAsc ? -1 : 1;
-        }
+			this.sortCol = col;
+			this.data.sort((a, b) => {
+				if (normalize(a[col]) < normalize(b[col])) {
+					return this.sortAsc ? -1 : 1;
+				}
 
-        if (normalize(a[col]) > normalize(b[col])) {
-          return this.sortAsc ? 1 : -1;
-        }
+				if (normalize(a[col]) > normalize(b[col])) {
+					return this.sortAsc ? 1 : -1;
+				}
 
-        return 0;
-      });
-    }
-  }));
+				return 0;
+			});
+		}
+	}));
 });
--- src/pages/constructors/constructors.11tydata.js
+++ src/pages/constructors/constructors.11tydata.js
@@ -2,9 +2,15 @@ const path = require('node:path');
 const loadJsonFromDir = require('../../_utils/loadJsonFromDir');
 
 module.exports = async () => {
-  const constructors = await loadJsonFromDir(
-    path.resolve(process.cwd(), `data/constructors`)
-  );
+	const constructors = await loadJsonFromDir(
+		path.resolve(process.cwd(), `data/constructors`)
+	);
 
-  return { constructors: Object.values(constructors) };
+	const data = Object.values(constructors).map((d) => ({
+		...d,
+		championshipWins: d.championshipStandings.filter((c) => c.position === 1)
+			.length
+	}));
+
+	return { constructors: data };
 };
