d7baa2974f42342d960cf32c6137f168ab0f07ac
Vivek Kumar Bansal <vkb0310@gmail.com>
2023-05-24 10:45:04 +0530

    chore: update packages

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-	output: 'server',
+	output: 'hybrid',
+	experimental: {
+		hybridOutput: true,
+	},
 	adapter: netlify(),
 });
